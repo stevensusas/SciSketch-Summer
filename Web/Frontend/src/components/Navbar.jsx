cfb9799c9550f16da2292b354f498e083f5290b9
ericzhy0815 <69284146+ericzhy0815@users.noreply.github.com>
2024-06-14 10:45:47 +0800

    style changes

--- Web/Frontend/src/components/Navbar.jsx
+++ Web/Frontend/src/components/Navbar.jsx
@@ -1,20 +1,5 @@
-import { Link } from "react-router-dom";
-
 const Navbar = () => {
-  return (
-    <nav className="top-navbar">
-      <div className="container">
-        <div className="brand">
-          <Link to="/">My Brand</Link>
-        </div>
-        <div className="menu">
-          <Link to="/about">About</Link>
-          <Link to="/services">Services</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-      </div>
-    </nav>
-  );
+  return <div></div>;
 };
 
 export default Navbar;
